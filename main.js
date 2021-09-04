bf90dc5c1e225d55f6f3d61d6d9c8368476e87dc
Bruyok Vlad <djkill911@gmail.com>
2021-09-04 17:23:48 +0300

    Code Edits

--- main.js
+++ main.js
@@ -4,33 +4,34 @@ const lastMessage = document.querySelector('.message');
 const container = document.querySelector('.project-container');
 const body = document.querySelector('.body');
 
+function noMessWindow() {
+    let noMessageWindow = document.createElement('div');
+    noMessageWindow.classList.add('no-message-window');
+    noMessageWindow.innerHTML = 'No Message!';
+    body.appendChild(noMessageWindow);
+}
+
+function timeOut() {
+    document.querySelector('.no-message-window').remove();
+}
+
+function createNewMess() {
+    let newMass = document.createElement('div');
+    newMass.classList.add('message-new');
+    newMass.innerHTML = entryField.value;
+    container.appendChild(newMass);
+}
+
 function addMessage() {
-    function deleteTextInput() {
-        entryField.value = event.target.value;
-    }
     if (entryField.value === "") {
-        function noMessWindow() {
-            let noMessageWindow = document.createElement('div');
-            noMessageWindow.classList.add('no-message-window');
-            noMessageWindow.innerHTML = 'No Message!';
-            body.appendChild(noMessageWindow);
-
-            function timeOut() {
-                document.querySelector('.no-message-window').remove();
-            }
-
-            setTimeout(timeOut, 2000);
-        }
-        noMessWindow()
+        noMessWindow();
+        setTimeout(timeOut, 2000);
     } else if (lastMessage.innerHTML === 'DO THIS PROJECT!') {
         lastMessage.innerHTML = entryField.value;
     } else {
-        let newMass = document.createElement('div');
-        newMass.classList.add('message-new');
-        newMass.innerHTML = entryField.value;
-        container.appendChild(newMass);
+        createNewMess();
     }
-    deleteTextInput()
+        entryField.value = event.target.value;
 }
 
 buttonSubmit.addEventListener('click', addMessage);
