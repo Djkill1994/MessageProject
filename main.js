400e5689d5a0e36cc4154b9a60e633f922f66dec
Bruyok Vlad <djkill911@gmail.com>
2021-09-04 16:37:30 +0300

    Message project v.1.0

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 const entryField = document.querySelector('.entry-field');
 const buttonSubmit = document.querySelector('.button-submit');
-const lastMessage = document.querySelector('.massage');
+const lastMessage = document.querySelector('.message');
 const container = document.querySelector('.project-container');
+const body = document.querySelector('.body');
 
-function addMessage (){
-    // entryField.value = event.target.value;
-    // if (entryField === ''){
-    //     buttonSubmit.setAttribute('disabled', 'disabled');
-    // }else{
-    //     buttonSubmit.removeAttribute('disabled', 'disabled');
-    // }
+function addMessage() {
+    function deleteTextInput() {
+        entryField.value = event.target.value;
+    }
+    if (entryField.value === "") {
+        function noMessWindow() {
+            let noMessageWindow = document.createElement('div');
+            noMessageWindow.classList.add('no-message-window');
+            noMessageWindow.innerHTML = 'No Message!';
+            body.appendChild(noMessageWindow);
 
-    // if (lastMessage === 'DO THIS PROJECT!'){
-    //     lastMessage.innerHTML = entryField.value;
-    // }else{
-        let newMass =document.createElement('h2');
-        newMass.classList.add('massage');
+            function timeOut() {
+                document.querySelector('.no-message-window').remove();
+            }
+
+            setTimeout(timeOut, 2000);
+        }
+        noMessWindow()
+    } else if (lastMessage.innerHTML === 'DO THIS PROJECT!') {
+        lastMessage.innerHTML = entryField.value;
+    } else {
+        let newMass = document.createElement('div');
+        newMass.classList.add('message-new');
         newMass.innerHTML = entryField.value;
         container.appendChild(newMass);
-    // }
+    }
+    deleteTextInput()
 }
-
 
 buttonSubmit.addEventListener('click', addMessage);
